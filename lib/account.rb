df4741dd55f812364bc70fa4031fc70db5b2e260
Inimesh <ijmeshoulam@gmail.com>
2022-01-31 16:56:15 +0000

    Priv method: Account#generate_record generates a new record of the transaction and stores it in @records attribute

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,10 @@
 class Account
   def initialize(init_balance=0)
-    @balance = init_balance
+    @records = []
+    @balance = 0; deposit(init_balance)
   end
 
+  # Commands
   def deposit(amount)
     @balance += amount
   end
@@ -10,11 +12,32 @@ class Account
   def withdraw(amount)
     @balance -= amount
   end
-  
+
+  # Queries
   def print_balance
     return @balance.to_s
   end
 
+  def print_statement
+    header = "date || credit || debit || balance\n"
+    record_string = @records.reverse_each.map{ |record|
+      record_to_string(record)
+    }
+    return header + record_string.join("\n")
+
+  end
+
   private
+  def gen_record(amount)
+    record = {
+      date: Time.now.strftime("%d/%m/%Y"),
+      credit: nil,
+      debit: nil,
+      balance: @balance
+    }
+    amount > 0 ? record[credit] = amount : record[debit] = amount
+
+    @records.append(record)
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -25,5 +25,24 @@ describe Account do
     end
   end
 
+  describe '#print_statement' do
+    it 'returns a full statement as a string' do
+      account = Account.new
+      allow(Time).to receive(:now).and_return(Time.new(2023, 1, 10))
+      account.deposit(1000)
+      allow(Time).to receive(:now).and_return(Time.new(2023, 1, 13))
+      account.deposit(2000)
+      allow(Time).to receive(:now).and_return(Time.new(2023, 1, 14))
+      account.withdraw(500)
+
+      expect(account.print_statement).to eq(
+        "date || credit || debit || balance
+        14/01/2023 || || 500.00 || 2500.00
+        13/01/2023 || 2000.00 || || 3000.00
+        10/01/2023 || 1000.00 || || 1000.00"
+      )
+    end
+  end
+
 
 end
