07d5964b8ff58ed72bd43f5f7b9da2bb61210f99
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 14:39:06 +0000

    Testing: fixing test for Record#generate_record

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -2,9 +2,8 @@ require 'record'
 
 describe Record do
   describe '#generate_record' do
-    it 'should generate a transaction record and append it to its store' do
-      record = Record.new
-      expect(record.generate_record).to change(record, :@store.length).by(1)
+    it 'should generate a transaction record and append it to its store attribute' do
+      expect { subject.generate_record(100, 1100) }.to change { subject.store.length }.by(1)
     end
   end 
 end
