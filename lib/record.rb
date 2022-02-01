cbac44c31b044ba5e13de4715bd4caf4d91e826f
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 16:12:55 +0000

    Fix: modifying Record#generate_record to check for non-zero amount value

--- lib/record.rb
+++ lib/record.rb
@@ -6,15 +6,18 @@ class Record
   end
 
   def generate_record(amount, balance)
-    record = {
-      date: Time.now.strftime("%d/%m/%Y"),
-      credit: nil,
-      debit: nil,
-      balance: balance
-    }
-    amount > 0 ? record[:credit] = amount : record[:debit] = -amount
-    @store.append(record)
-    return record
-  end
+    if amount != 0
+      record = {
+        date: Time.now.strftime("%d/%m/%Y"),
+        credit: nil,
+        debit: nil,
+        balance: balance
+      }
+      amount > 0 ? record[:credit] = amount : record[:debit] = -amount
+      @store.append(record)
+      return record
+    end
+    return false
 
+  end
 end
