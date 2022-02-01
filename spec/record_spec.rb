0ad76a131ade5bcca9d7f949c10d6be36db898bc
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 18:30:44 +0000

    Adding TODO comment to record_spec.rb

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -3,6 +3,7 @@ require 'record'
 describe Record do
   describe '#generate_record' do
     it 'should generate a transaction record and append it to its store attribute' do
+      # TODO Check that Time.now is called
       expect { subject.generate_record(100, 1100) }.to change { subject.store.length }.by(1)
     end
   end 
