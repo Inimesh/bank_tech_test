18167fd9f1a14af814cbd9563fa62b27e2f319ab
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 18:30:14 +0000

    Testing: re-writing tests to now test extracted class

--- spec/typesetter_spec.rb
+++ spec/typesetter_spec.rb
@@ -11,8 +11,13 @@ describe Typesetter do
       # account.deposit(2000)
       # allow(Time).to receive(:now).and_return(Time.new(2023, 1, 14))
       # account.withdraw(500)
+      record = [
+        {date: '10/01/2023', credit: 1000, debit: nil, balance: 1000},
+        {date: '13/01/2023', credit: 2000, debit: nil, balance: 3000},
+        {date: '14/01/2023', credit: nil, debit: 500, balance: 2500}
+      ]
 
-      expect(account.statement).to eq(
+      expect(described_class.statement(record)).to eq(
         <<~HEREDOC.chomp
         date || credit || debit || balance
         14/01/2023 || || 500.00 || 2500.00
@@ -25,8 +30,8 @@ describe Typesetter do
 
   describe '.balance' do
     it 'should display the current balance to 2 decimal places' do
-      account = Account.new(100)
-      expect(account.balance).to eq('100.00') 
+      balance = 100
+      expect(described_class.balance(balance)).to eq('100.00') 
     end
   end
   
