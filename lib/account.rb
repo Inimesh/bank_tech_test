8a36f426455b19a2d42dbd5a4694f33b82689a29
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 15:06:55 +0000

    Refactor: Dependency injecting Record.new into Account. Rewriting methods that now use Record.new

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,20 @@
+require 'record.rb'
+
 class Account
-  def initialize(init_balance=0)
-    @records = []
+  def initialize(init_balance=0, record_obj=Record.new)
+    @record = record_obj
     @balance = 0.00; deposit(init_balance) if init_balance != 0
   end
 
   # Commands
   def deposit(amount)
     @balance += amount
-    gen_record(amount)
+    @record.generate_record(amount, @balance)
   end
 
   def withdraw(amount)
     @balance -= amount
-    gen_record(-amount)
+    @record.generate_record(-amount, @balance)
   end
 
   # Queries
