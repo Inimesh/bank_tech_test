34077c38e746a5d50e2902f2a0a7fc4778e50a71
Inimesh <ijmeshoulam@gmail.com>
2022-02-01 16:15:04 +0000

    Testing: re-writing testing for Account to use an instance_double of the dependency injected Record instance, and for the test to check for a method call on the instance double

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,6 @@
 
 require 'account'
+require 'record'
 
 describe Account do
   describe '#balance' do
@@ -9,19 +10,40 @@ describe Account do
     end
   end
 
+  # before(:each) do
+  #   record = instance_double("Record", generate_record: true)
+  #   account = Account.new(0, record)
+  # end
+
   describe '#deposit' do
     it 'should add the specified amount to the balance' do
-      account = Account.new
+      record = instance_double("Record", generate_record: true)
+      account = Account.new(0, record)
       account.deposit(120)
       expect(account.balance).to eq('120.00')
+    end
+    
+    it 'should call its instance of Record to generate a record' do
+      record = instance_double("Record", generate_record: true)
+      account = Account.new(0, record)
+      expect(record).to receive(:generate_record)
+      account.deposit(120)
     end
   end
   
   describe '#withdraw' do
     it 'should deduct the specified amount from the balance' do
-      account = Account.new(100)
+      record = instance_double("Record", generate_record: true)
+      account = Account.new(0, record)
       account.withdraw(50)
-      expect(account.balance).to eq('50.00')
+      expect(account.balance).to eq('-50.00')
+    end
+
+    it 'should call its instance of Record to generate a record' do
+      record = instance_double("Record", generate_record: true)
+      account = Account.new(0, record)
+      expect(record).to receive(:generate_record)
+      account.withdraw(50)
     end
   end
 
